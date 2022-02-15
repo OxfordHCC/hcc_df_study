8cf6bdfb841e9cc8516277633e204a4d78ddf298
treebirg <treebirg@gmail.com>
2022-02-15 15:20:43 +0000

    remove old splitOf function from fp

--- game/server/src/lib/fp.ts
+++ game/server/src/lib/fp.ts
@@ -3,15 +3,3 @@ export type Either<L, R> = L | R;
 export function filterOf<T>(arr: any[], constructor: Function): T[]{
 	return arr.filter(x => x instanceof constructor);
 }
-
-export function splitOf<L,R>(
-	arr: Either<L,R>[],
-	leftConstructor: Constructor<L>,
-	rightConstructor: Constructor<R>
-): [L[], R[]]{
-	return [
-		filterOf<L>(arr, leftConstructor),
-		filterOf<R>(arr, rightConstructor),
-	];
-}
-
