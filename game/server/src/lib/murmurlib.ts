8a85ed86778ac975531d2e50ca4678a57e2579b3
treebirg <treebirg@gmail.com>
2022-02-17 21:46:07 +0000

    restore murmur container functions in separate murmurlib module

---
+++ game/server/src/lib/murmurlib.ts
@@ -0,0 +1,44 @@
+import { dockerReq, start } from './dockerlib';
+import { Either } from './fp';
+
+type CreateMurmurContainerParams = {
+	grpcPort: number;
+	murmurPort: number;
+	name?: string
+}
+type CreateMurmurContainerResult = {
+	Id: string,
+	Warning: unknown[]
+}
+export function createMurmurContainer(
+	{ grpcPort, murmurPort, name }: CreateMurmurContainerParams
+): Promise<Either<Error,CreateMurmurContainerResult>> {
+	return dockerReq(
+		"POST",
+		"/containers/create",
+		{
+			name
+		},
+		{
+			Image: "mumble_server",
+			ExposedPorts: {
+				"64738/tcp": {},
+				"64738/udp": {},
+				"50051": {}
+			},
+			HostConfig: {
+				PortBindings: {
+					"64738/udp": [{
+						HostPort: murmurPort.toString()
+					}],
+					"64738/tcp": [{
+						HostPort: murmurPort.toString()
+					}],
+					"50051": [{
+						HostPort: grpcPort.toString()
+					}]
+				}
+			}
+		}
+	);
+}
