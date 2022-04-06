e0cdaf7fc70817fa68e28d5861f26fd357ece9b5
treebirg <treebirg@gmail.com>
2022-04-06 22:25:59 +0100

    add Future-returning exec function (child_process)

--- server/src/lib/util.ts
+++ server/src/lib/util.ts
@@ -1,8 +1,30 @@
 import { FutureInstance, Future } from 'fluture';
 import { Either, Right, Left, List } from 'monet';
+import { exec } from 'child_process';
 
-export function sort<T>(fn: (a:T, b: T) => number): (elements: T[]) => T[]{
-	return function(elements){
+// execFuture
+// TODO: Move to separate file (utils probably)
+// for now, only utf8 output is supported
+// this makes some things easier wrt to type checking (eg. string vs buffer)
+export function execF(command: string): FutureInstance<Error, string>{
+	return Future((rej, res) => {
+		exec(command, (err, stdout, stderr) => {
+			if(err !== null){
+				return rej(err);
+			}
+			if(stderr.length > 0){
+				return rej(new Error(stderr));
+			}
+			return res(stdout);
+		});
+		
+		return ()=>{};
+	});
+}
+
+
+export function sort<T>(fn: (a: T, b: T) => number): (elements: T[]) => T[] {
+	return function(elements) {
 		return elements.sort(fn);
 	}
 };
@@ -46,4 +68,5 @@ function a2list<T>(arr: Array<T>): List<T>{
 export function aoe2ea<E,T>(arr: Array<Either<E,T>>): Either<E, Array<T>>{
 	return a2list(arr).sequenceEither<E,T>().map(list2a);
 }
+
 
