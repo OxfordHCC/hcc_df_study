7f1721a5570bba8bebdb3bfb064ba413d69582f9
jimtarded <jackjacksoncs@gmail.com>
2022-08-25 14:56:38 +0100

    testing whether tutorial rounds are the cause of error 'E:1661434002741:session:create_session:Unknown round name.' thrown by Aloysius when attempting to create a session.

--- server/src/const/tutorialSchedules.ts
+++ server/src/const/tutorialSchedules.ts
@@ -3,19 +3,19 @@ import { ConcreteRoundData } from 'dfs-common';
 export const tutorialSchedules: ConcreteRoundData[][] = [
 	[
 		{
-			name: "tutorial-button",
+			name: "button",
 			solution: 0,
 			msLength: 60000,
 			options: [0, 1, 2]
 		},
 		{
-			name: "tutorial-button",
+			name: "button",
 			solution: 1,
 			msLength: 60000,
 			options: [0, 1, 2]
 		},
 		{
-			name: "tutorial-button",
+			name: "button",
 			solution: 2,
 			msLength: 60000,
 			options: [0, 1, 2]
@@ -23,19 +23,19 @@ export const tutorialSchedules: ConcreteRoundData[][] = [
 	],
 	[
 		{
-			name: "tutorial-button",
+			name: "button",
 			solution: 2,
 			msLength: 60000,
 			options: [0, 1, 2]
 		},
 		{
-			name: "tutorial-button",
+			name: "button",
 			solution: 0,
 			msLength: 60000,
 			options: [0, 1, 2]
 		},
 		{
-			name: "tutorial-button",
+			name: "button",
 			solution: 1,
 			msLength: 60000,
 			options: [0, 1, 2]
